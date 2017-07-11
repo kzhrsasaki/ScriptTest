01e0c24be7501fe6fb63813a94632b1a5fd469ce
kzhr.sasaki <kzhr.sasaki@gmail.com>
2017-07-11 15:57:36 +0900

    Lesson4課題の再提出分です。
    よろしくお願いします。

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -31,7 +31,7 @@ public class Test : MonoBehaviour {
 		points [4] = 80;
 
 		// 配列の各要素の値を逆順に表示する
-		for (int i = 0; i < 5; i=4-i) {
+		for (int i = 4; i >= 0; i--) {
 			Debug.Log (points [i]);
 		}
 
